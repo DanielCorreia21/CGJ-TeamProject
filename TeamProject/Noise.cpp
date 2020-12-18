a7cdab4a880393f6c8dfc3248ad6363ce1c2b962
apontallier <antoine.pontallier@gmail.com>
2020-12-18 10:04:41 +0100

    idk

--- TeamProject/Noise.cpp
+++ TeamProject/Noise.cpp
@@ -20,3 +20,23 @@ double PerlinNoise::noise2D(double x, double y, double alpha, double beta, int n
 	}
 	return(sum);
 }
+
+double PerlinNoise::noise3D(double x, double y, double z, double alpha, double beta, int n)
+{
+	int i;
+	double sum = 0;
+	double p[3], scale = 1;
+
+	p[0] = x;
+	p[1] = y;
+	p[2] = z;
+	for (i = 0; i < n; i++) {
+		double val = perlin::noise3(p);
+		sum += val / scale;
+		scale *= alpha;
+		p[0] *= beta;
+		p[1] *= beta;
+		p[2] *= beta;
+	}
+	return(sum);
+}
--- TeamProject/Noise.h
+++ TeamProject/Noise.h
@@ -4,4 +4,5 @@ class PerlinNoise
 {
 public:
 	double noise2D(double x, double y, double alpha, double beta, int n);
+	double noise3D(double x, double y, double z, double alpha, double beta, int n);
 };
